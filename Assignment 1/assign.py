5e5f88fb1427a6314cbbd91c583888d138aa00f7
Anushka2324 <155560984+Anushka2324@users.noreply.github.com>
2025-08-10 13:00:12 +0530

    Add files via upload

---
+++ Assignment 1/assign.py
@@ -0,0 +1,22 @@
+print("assignment operator")
+
+a = 3
+b = 10
+
+a += b
+print("a+b : ", a)
+
+a-=b
+print('a-b : ',a)
+
+a*=b
+print("a*b : ", a)
+
+a/=b
+print("a/b : ", a)
+
+a%=b
+print("a%b : ", a)
+
+
+
---
+++ Assignment 1/comparison.py
@@ -0,0 +1,12 @@
+print("comparison operator")
+
+a = 2
+b = 5
+
+if(a>=b) :
+	print("a is greater than b ")
+elif(a<=b) :
+	print("a is smaller than b ")
+elif(a==b):
+	print("a and b are equal")	
+	
