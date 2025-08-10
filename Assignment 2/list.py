86126502533c4df19491ad13ac2f58f904148e22
Anushka2324 <155560984+Anushka2324@users.noreply.github.com>
2025-08-10 19:48:00 +0530

    Add files via upload

---
+++ Assignment 2/list.py
@@ -0,0 +1,17 @@
+
+ls = [25,"Anushka",21,89.70]
+print("list : ",ls)
+print("length of list : ",len(ls))
+ls.append("dypcet")
+print("list after append : ",ls)
+ls.insert(2,"Patil")
+print("list after insert : ",ls)
+ls.remove(21)
+print("list after remove : ",ls)
+ls.pop()
+print("list after pop : ",ls)
+ls1 = [10,50,10.50,30,20,40.60]
+ls1.sort()
+print("list after sort : ",ls1)
+ls.count(62)
+print("list count function : ",ls)
---
+++ Assignment 2/tuple.py
@@ -0,0 +1,4 @@
+tp = (25,"Anushka",21,89.70)
+print("tuple",tp)
+print("index function : ",tp.index(21))
+print("count function : ",tp.count(21))
