507829ad52911a876ecd2341996fcfd6552dac68
Anushka2324 <155560984+Anushka2324@users.noreply.github.com>
2025-08-11 20:03:45 +0530

    Add files via upload

---
+++ Assignment 4/even.py
@@ -0,0 +1,17 @@
+ 
+def func(l,n) :
+	for j in range(0,n):
+		if(l[j]%2 == 0):
+			print(l[j])
+
+n = int(input("how many numbers you want in list : "))
+
+l= []
+
+print("enter numbers : ")
+for i in range(0,n) :
+	num = int(input())
+	l.append(num)
+	
+print("even numbers are : ")
+func(l,n)	
